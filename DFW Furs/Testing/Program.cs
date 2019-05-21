dc9ed0efce8d7aba51022ac4dce6554d1c85bf65
parabola949 <parabola949@gmail.com>
2019-05-21 16:01:48 -0500

    initial commit

---
+++ DFW Furs/Testing/Program.cs
@@ -0,0 +1,14 @@
+﻿using DFW.Furs.Api;
+using System;
+
+namespace Testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var posts = Telegram.ParseChannel("DFWEvents").Result;
+            Console.Read();
+        }
+    }
+}
