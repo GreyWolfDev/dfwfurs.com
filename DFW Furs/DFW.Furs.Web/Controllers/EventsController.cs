e338b600992e86248aef4a865abf57a1784d6228
parabola949 <parabola949@gmail.com>
2019-05-29 15:25:46 -0500

    new logo, added some (blank) pages

---
+++ DFW Furs/DFW.Furs.Web/Controllers/EventsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DFW.Furs.Web.Controllers
+{
+    public class EventsController : Controller
+    {
+        public IActionResult Calendar()
+        {
+            return View();
+        }
+        public IActionResult Descriptions()
+        {
+            return View();
+        }
+        public IActionResult Organizers()
+        {
+            return View();
+        }
+        public IActionResult Photos()
+        {
+            return View();
+        }
+    }
+}
--- DFW Furs/Testing/Program.cs
+++ DFW Furs/Testing/Program.cs
@@ -8,6 +8,14 @@ namespace Testing
         static void Main(string[] args)
         {
             var posts = Telegram.ParseChannel("DFWEvents").Result;
+            posts.Reverse();
+            foreach (var p in posts)
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.Write($"{p.MessageAuthor}: ");
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.WriteLine($"{p.Text}\r\n");
+            }
             Console.Read();
         }
     }
