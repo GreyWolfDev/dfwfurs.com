508d8ae77c5c8612b9246c9a4846102d5bab0f58
parabola949 <parabola949@gmail.com>
2019-05-31 09:00:35 -0500

    move to .NET Core 2.2

--- DFW Furs/DFW.Furs.Web/ApiControllers/TelegramController.cs
+++ DFW Furs/DFW.Furs.Web/ApiControllers/TelegramController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using DFW.Furs.Api;
 using DFW.Furs.Models;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,12 +12,14 @@ namespace DFW.Furs.Web.ApiControllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [EnableCors]
     public class TelegramController : ControllerBase
     {
         [HttpGet]
         public async Task<List<TgChannelPost>> Get(string channel = "DFWEvents", int total = 3)
         {
             //TODO: Add error handling
+            //TODO: Move this to greywolfdev.net
             return await Telegram.ParseChannel(channel, total);
         }
     }
