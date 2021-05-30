e2a93e17dd90f62ece3236bd4fc65473bb493eec
parabola949 <parabola949@gmail.com>
2021-05-30 13:22:32 -0500

    Attempt to show error page

--- DFW Furs/DFW.Furs.Web/Startup.cs
+++ DFW Furs/DFW.Furs.Web/Startup.cs
@@ -66,15 +66,15 @@ namespace DFW.Furs.Web
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
+            //if (env.IsDevelopment())
+            //{
                 app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                app.UseHsts();
-            }
+            //}
+            //else
+            //{
+            //    app.UseExceptionHandler("/Home/Error");
+            //    app.UseHsts();
+            //}
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
